95f600f1feac91cd76117a52f8b8c1841bd35058
alcoimbra88 <al_coimbra@hotmail.com>
2021-01-18 19:06:53 -0300

    Commit Inicial

---
+++ javascript/inicio.js
@@ -0,0 +1,21 @@
+function mouseEncima (elemento){
+    elemento.style.textShadow="0px 0px 8px white";
+    elemento.style.fontSize="3.2vh";
+    elemento.style.transition="0.8s";
+}
+
+function mouseFora (elemento){
+    elemento.style.textShadow="none";
+    elemento.style.fontSize="3vh";
+    elemento.style.transition="0.8s"
+}
+
+function mouseEncimaFooter (elemento){
+    elemento.style.color="rgb(114, 114, 231)"
+    elemento.style.transition="0.8s"
+}
+
+function mouseForaFooter (elemento){
+    elemento.style.color="white"
+    elemento.style.transition="0.8s"
+}
---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+function mouseencima (elemento){
+    elemento.style.textShadow="0px 0px 8px white";
+    elemento.style.transition="0.8s"
+}
+
+function mousefora (elemento){
+    elemento.style.textShadow="none";
+    elemento.style.transition="0.8s"
+}
