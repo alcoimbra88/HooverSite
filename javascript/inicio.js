5b10d93257b2bcaeb96fb1d5ec2a54aa5fb0a099
alcoimbra88 <al_coimbra@hotmail.com>
2021-01-25 21:35:15 -0300

    Finalizacao div inicio e criacao da div transicao-01

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -40,9 +40,13 @@ function navBarScroll (elemento){
 }
 
 
-/*================= teste ====================*/
+/*================= Event Scroll Nav-Bar ====================*/
 
     window.addEventListener("scroll",function() {
         var nav = document.querySelector("nav");
         nav.classList.toggle("sticky" , window.scrollY > 0);
     })
+
+
+
+  
